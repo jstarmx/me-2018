53c988f6f2fda99e912a18a27afa897f37aef43c
jstarmx <jstar.mx@gmail.com>
2018-08-09 22:20:34 +0100

    Add webpack config for building sass

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './src/styles/app.scss',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loader: 'import-glob-loader',
+        enforce: 'pre',
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'postcss-loader', 'sass-loader'],
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin('./styles/app.css'),
+    new CopyWebpackPlugin([
+      { from: './src/images', to: path.resolve(__dirname, 'dist/images') },
+    ]),
+  ],
+};
