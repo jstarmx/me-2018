bba860518bbdcec06991c17c74e73917147feee8
jstarmx <jstar.mx@gmail.com>
2019-04-18 22:07:09 +0100

    Add lightbox script

---
+++ src/scripts/lightbox.js
@@ -0,0 +1,30 @@
+const Lightbox = (function Lightbox() {
+  const render = path => `
+    <img src="/images/x.svg" class="lightbox__x" alt="close" />
+    <img src="${path}" class="lightbox__image" role="presentation" />
+  `;
+
+  return {
+    init() {
+      document.querySelectorAll('.thumb__link').forEach(thumb => {
+        thumb.addEventListener('click', event => {
+          event.preventDefault();
+          const path = event.target.getAttribute('href');
+          const lightbox = document.createElement('div');
+          lightbox.className = 'lightbox';
+          lightbox.innerHTML = render(path);
+          lightbox
+            .querySelector('.lightbox__x')
+            .addEventListener('click', () => {
+              lightbox.remove();
+            });
+          document.body.appendChild(lightbox);
+        });
+      });
+    },
+  };
+})();
+
+window.addEventListener('DOMContentLoaded', () => {
+  Lightbox.init();
+});
